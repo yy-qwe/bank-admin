e770dc567a263841607a110e85994843d0768e33
yy <2639566052@qq.com>
2021-05-16 17:36:32 +0800

    github page fail:change main test

--- .history/src/main_20210516150555.js
+++ .history/src/main_20210516150555.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './permission'
-/* import { mockXHR } from '../mock'
+import { mockXHR } from '../mock'
 
 if (process.env.NODE_ENV === 'production') { mockXHR() }
- */
+
 Vue.config.productionTip = false
 
 new Vue({
